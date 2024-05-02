e79212a526f1fc4799cbd3a87c0e2f0dc3915434
Nikolay Ershov <n1k.ersh@yandex.ru>
2024-05-02 12:33:51 +0300

    added url to Stripe payment in admin panel

--- order_app/admin.py
+++ order_app/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Order, OrderItem
+
+
+def order_stripe_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+    return ''
+
+
+order_stripe_payment.short_description = 'Stripe payment'
 
 
 class OrderItemInline(admin.TabularInline):
@@ -10,7 +22,7 @@ class OrderItemInline(admin.TabularInline):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created',
-                    'updated']
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid',
+                    order_stripe_payment, 'created','updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
